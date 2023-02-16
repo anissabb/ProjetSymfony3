9f9993f67cbca58d726eac2cdc9c3d24637bf184
Anissa Bojabah <a.bojabah@interface3.be>
2023-02-16 11:54:51 +0100

    ok

--- src/Controller/ExemplesUtilisationController.php
+++ src/Controller/ExemplesUtilisationController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ExemplesUtilisationController extends AbstractController
 {
     #[Route('/exemples/utilisation/afficher/tous', name: 'afficher_tous')]
-    public function afficherTous(): Response
+    public function afficherTous(HttpClientInterface $client): Response
     {
+        $response=$client->request(
+            'GET',
+            'http://localhost:8000/exemples/utilisation/afficher/tous'
+        );
+        dd($response->getContent());
         return $this->render('exemples_utilisation/afficher_tous.html.twig');
     }
 }
